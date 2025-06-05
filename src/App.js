b7c58df1224f2a84ce8237e811dee073264073be
Marcos676 <patino-marcos@outlook.com>
2025-06-05 18:35:32 -0300

    diseño terminado

--- src/App.js
+++ src/App.js
@@ -7,53 +7,73 @@ function App() {
   const [discount, setDiscount] = useState("");
   const [finalPrice, setFinalPrice] = useState("");
 
+
+
   const handleValues = (price, disc) => {
     //Actualizacion de valores
     setOriginalPrice(price);
     setPorcentDiscount(disc);
 
     //si algun input esta vacío no muestra ningun resultado
-    if (price === "" || disc === "") {//si un input esta vacío devuelve un string vacío
+    if (price === "" || disc === "") {
+      //si un input esta vacío devuelve un string vacío
       setDiscount("");
       setFinalPrice("");
-    } else {      
+    } else {
       setDiscount((price * disc) / 100);
-      setFinalPrice(price - ( (price * disc) / 100 ));
+      setFinalPrice(price - (price * disc) / 100);
     }
   };
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <div><i class="fa-solid fa-bookmark"></i></div>
+        <i className="fa-regular fa-bookmark"></i>
+        <i className="fas fa-info-circle"></i>
       </header>
       <main>
         <div className="title-description">
-          <h1>Título</h1>
-          <p>Descripción de uso</p>
+          <h2>Calculá tu descuento al instante</h2>
         </div>
-        <div className="functional-app">
+        <form className="functional-app">
           <div className="inputs-box">
+            <label htmlFor="original-price">Precio</label>
             <input
+              id="original-price"
               className="original-price"
               type="number"
               min={0}
-              placeholder="$ Precio"
+              placeholder="$"
               onInput={(e) => handleValues(e.target.value, porcentDiscount)}
             />
+          </div>
+          <div className="inputs-box">
+            <label htmlFor="porcent-to-discount">Descuento</label>
             <input
+              id="porcent-to-discount"
               className="porcent-to-discount"
               type="number"
               min={0}
               max={100}
-              placeholder="% Descuento"
+              placeholder="%"
               onInput={(e) => handleValues(originalPrice, e.target.value)}
             />
-            %
           </div>
-          <p className="discount">Descuento: {discount}</p>
-          <p className="final-price">Precio final: {finalPrice}</p>
-        </div>
+          <div>
+            <p className="discount">
+              <i className="fas fa-arrow-right result-icons"></i>
+               Ahorro: $
+              <span className="number-discount">{discount}</span>
+            </p>
+            <p className="final-price">
+              <i className="fa-solid fa-sack-dollar result-icons"></i>
+               Precio final: $
+              {finalPrice}
+            </p>
+          </div>
+          <button type="button">Guardar cálculo</button>
+        </form>
       </main>
     </div>
   );
